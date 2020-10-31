b0e36881b30fd7014c8f83d0c83b4d4cba0a05e0
Ollidebolli <43846632+Ollidebolli@users.noreply.github.com>
2020-10-31 15:14:26 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 mkdir -p ~/.streamlit/
 echo "\
 [general]\n\
-email = \"oliverdeboermoran@gmail.com\"\n\
+email = \"oliver.de.boer.moran@sap.com\"\n\
 " > ~/.streamlit/credentials.toml
 echo "\
 [server]\n\
